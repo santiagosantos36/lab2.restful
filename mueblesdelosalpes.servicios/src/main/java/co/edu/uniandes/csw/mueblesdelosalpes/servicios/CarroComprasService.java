327c66beccdee60b03541b5123f4552fb29d0038
santiago <santiagosantose101@gmail.com>
2021-04-11 17:58:21 -0500

    VERSION 1: LAB2 RESTFUL

--- mueblesdelosalpes.servicios/src/main/java/co/edu/uniandes/csw/mueblesdelosalpes/servicios/CarroComprasService.java
+++ mueblesdelosalpes.servicios/src/main/java/co/edu/uniandes/csw/mueblesdelosalpes/servicios/CarroComprasService.java
@@ -16,6 +16,7 @@ import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
  
@@ -65,5 +66,11 @@ public class CarroComprasService {
         return carroEjb.getInventario();
  
     }
+    
+   
+    
+    
+    
+    
  
 }
